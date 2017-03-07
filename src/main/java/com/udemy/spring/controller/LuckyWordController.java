94cf4173ff599ba36c60cf1987f3dcfa01530d61
carlos.cortes <carlos.cortes@aracom.de>
2017-03-07 15:34:08 +0100

    Configuration initial

---
+++ src/main/java/com/udemy/spring/controller/LuckyWordController.java
@@ -0,0 +1,18 @@
+package com.udemy.spring.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RestController
+@RefreshScope
+public class LuckyWordController {
+	 
+	@Value("${lucky-word}") String luckyWord;
+	  
+	  @RequestMapping("/lucky-word")
+	  public String showLuckyWord() {
+	    return "The lucky word is: " + luckyWord;
+	  }
+}
